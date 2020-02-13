1efc59292ca748e7cf86cbc4a7a032a3f083dad2
Eugene <etanshs0@gmail.com>
2020-02-12 21:47:17 -0800

    Initial Commit

---
+++ Geolocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Geolocation
+//
+//  Created by Eugene Tan on 2/12/20.
+//  Copyright © 2020 Eugene Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
