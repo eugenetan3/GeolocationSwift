aeddf6b5876b2a7e0b18b5d3c31871e55fd90917
Eugene Tan <44623905+eugenetan3@users.noreply.github.com>
2020-03-11 13:06:54 -0700

    Added files via upload
    Build

--- Geolocation/ViewController.swift
+++ Geolocation/ViewController.swift
@@ -31,8 +31,51 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         if let location = locations.first {
             print(location.coordinate)
             print(location.speed)
+            
+            let parameters: [String : Double] = ["Longitude": location.coordinate.longitude, "Latitude": location.coordinate.latitude, "Speed": location.speed, "Direction": 0.005]
+            
+            let url = URL(string: "http://127.0.0.1:5000/post-requests")!
+            
+            let session = URLSession.shared
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            
+            do {
+                request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            
+            let task = session.dataTask(with: request, completionHandler: { data, response, error in
+
+                guard error == nil else {
+                    return
+                }
+
+                guard let data = data else {
+                    return
+                }
+
+                do {
+                    //create json object from data
+                    if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                        print(json)
+                        // handle json...
+                    }
+
+                } catch _ {
+                   // print(error.localizedDescription)
+                }
+            })
+            
+            task.resume()
+            
+            }
         }
-    }
+    
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if (status == CLAuthorizationStatus.denied) {
